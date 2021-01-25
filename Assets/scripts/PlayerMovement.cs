62f12eda6d77cdf55a20c3e1795abb93b4978aa0
unknown <victoria.goette01@htl-salzburg.ac.at>
2021-01-25 10:15:20 +0100

    Game ready, Start Menu and Back to menu

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -10,6 +10,14 @@ public class PlayerMovement : MonoBehaviour
     public Vector3 playerPos;
     public float speed = 0.1f;
     // Start is called before the first frame update
+
+    public float minScale;
+    public float maxSccale;
+    public float scale;
+
+    public Transform minHeight;
+    public Transform maxHeight;
+
     void Start()
     {
         playerPos = thisTransform.position;
@@ -19,6 +27,11 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         thisTransform.position = Vector3.MoveTowards(thisTransform.position, playerPos, speed);
+        //  (0.1,0.4,0.1)
+        float value = Mathf.InverseLerp(minHeight.position.y, maxHeight.position.y, thisTransform.position.y);
+        scale = Mathf.Lerp(minScale, maxSccale, value);
+        Vector3 scaleVector = new Vector3(scale, scale, scale);
+        thisTransform.localScale = scaleVector;
     }
 
     public void SetNewDestination()
